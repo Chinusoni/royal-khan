94873cae3504bec36ca8f7109332398b9d375ec3
Harsh <your-soniverma2006@gmail.com>
2026-03-09 00:09:32 +0530

    Configured project for Vercel deployment

--- royal_khan/settings.py
+++ royal_khan/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&2uh!7-z21ymfo78_@&3k@oi2d)_wgft54t^!+us=lyvy15jad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
--- royal_khan/wsgi.py
+++ royal_khan/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'royal_khan.settings')
 
 application = get_wsgi_application()
+app = application
